de3368fa82899311e8cb14b823534ddaabc9b861
Lalaso2000 <raraso2000@icloud.com>
2018-04-13 13:48:46 +0900

    【修正】ワーカーの扱いを変更

--- AnalogSimulator/src/analogsimulator/Player.java
+++ AnalogSimulator/src/analogsimulator/Player.java
@@ -16,38 +16,35 @@ public class Player {
     // プレイヤーのクラス
 
     // ワーカーの種類
-    public static final int WK_DOCTOR = 1;
-    public static final int WK_ASSISTANT = 2;
-    public static final int WK_STUDENT = 3;
+    public static final int WK_DOCTOR = 0;
+    public static final int WK_ASSISTANT = 1;
+    public static final int WK_STUDENT = 2;
 
-    public String name;                        // 自分の名前
-    public GameBoard board;                    // 自分がいるボード
-    public ArrayList<Integer> workers;         // 持ちワーカーのリスト
-    public ArrayList<Integer> remainWorkers;   // 手元にいる持ちワーカー(ボードに出すと減る)
-    public int money;                          // 資金
-    public int flasks;                         // フラスコ(研究成果)
-    public int allStars;                       // 星(研究業績)の総和
+    public String name;                         // 自分の名前
+    public GameBoard board;                     // 自分がいるボード
+    public int workers[] = {1,0,1};             // ワーカー人数
+    private int remainWorkers[] = {1,0,1};       // 手元のワーカー人数
+    public int money = 5;                       // 資金
+    public int flasks = 0;                      // フラスコ(研究成果)
+    public int allStars = 0;                    // 星(研究業績)の総和
 
     public Player(GameBoard gb, String n) {
         board = gb;
         name = n;
-        workers = new ArrayList<>(Arrays.asList(WK_DOCTOR, WK_STUDENT));
-        remainWorkers = (ArrayList<Integer>) workers.clone();
-        money = 5;
-        flasks = 0;
-        allStars = 0;
+
     }
 
     public void showStatus() {
-        System.out.print("workers : ");
-        for (Integer worker : workers) {
-            System.out.print(worker + ",");
-        }
-        System.out.println("");
+        System.out.println("workers : " + workers);
         System.out.println("money : " + money);
         System.out.println("flasks : " + flasks);
         System.out.println("allStars : " + allStars);
     }
     
+    public boolean putWorker(int workerKind){
+        if(remainWorkers[workerKind] == 0) return false;
+        remainWorkers[workerKind] -= 1;
+        return true;
+    }
 
 }
