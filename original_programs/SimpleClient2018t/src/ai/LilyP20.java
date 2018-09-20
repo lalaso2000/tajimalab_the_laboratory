908daf95ba5672a9c734d552430d137b01d1114c
lalaso2000 <raraso2000@icloud.com>
2018-09-21 00:26:09 +0900

    【追加】Lily-P20 実行ファイル追加 (微リファクタリング)

--- original_programs/SimpleClient2018t/src/ai/LilyP20.java
+++ original_programs/SimpleClient2018t/src/ai/LilyP20.java
@@ -12,8 +12,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 
 /**
  * LilyP20<br>
