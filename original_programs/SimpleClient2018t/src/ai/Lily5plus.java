561b737ffea4b00b4d6ae69fc90b0126df3a1fbc
lalaso2000 <raraso2000@icloud.com>
2018-09-22 15:09:37 +0900

    【完成】Lily ∞

--- original_programs/SimpleClient2018t/src/ai/Lily5plus.java
+++ original_programs/SimpleClient2018t/src/ai/Lily5plus.java
@@ -80,7 +80,7 @@ public class Lily5plus extends TajimaLabAI {
      */
     public Lily5plus(Game game) {
         super(game);
-        this.myName = "Lily5p v1.01";
+        this.myName = "Lily ∞";
         this.mode = NORMAL_MODE;
     }
 
